d9efd7879da679bf05c12d8272d724ae26197ff1
Marwan YOUMNI <95295521+Marwwannn@users.noreply.github.com>
2024-01-02 16:42:32 +0100

    ContratLoc
    il y a un pb ça m'affiche null mais apres tout marche

--- src/controle/GestionVoirContratLocation.java
+++ src/controle/GestionVoirContratLocation.java
@@ -39,9 +39,15 @@ public class GestionVoirContratLocation implements ActionListener {
             JButton button = (JButton) source;
 
             switch (button.getText()) {
-                case "Valider":
+                case "Voir cette facture":
 				;
-				contratLocataire.dispose();
+				
+				try {
+					afficherContratLoc();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
                     break;
                 case "Annuler":
                 	contratLocataire.dispose();
@@ -50,7 +56,7 @@ public class GestionVoirContratLocation implements ActionListener {
         }
     }
     
-    public void afficherLocataire() throws SQLException {
+    public void afficherContratLoc() throws SQLException {
 		Collection<ContratLocation> ContratLocation = daoContratLocation.findAll();
         DefaultTableModel tableModel = (DefaultTableModel) contratLocataire.getTable().getModel();
 
@@ -59,18 +65,29 @@ public class GestionVoirContratLocation implements ActionListener {
 
         // Populate the table model with data
         int ligne = 0;
-        for (ContratLocation contLoc : locataires) {
+        for (ContratLocation contLoc : ContratLocation) {
             if (tableModel.getRowCount() <= ligne) {
                 // Add a new row if needed
                 tableModel.addRow(new Object[0]);
             }
 
-            tableModel.setValueAt(loc.getNom(), ligne, 0);
-            tableModel.setValueAt(loc.getPrenom(), ligne, 1);
-            tableModel.setValueAt(loc.getTelephone(), ligne, 2);
-            tableModel.setValueAt(loc.getMail(), ligne, 3);
-            tableModel.setValueAt(loc.getAdresse(), ligne, 4);
-            tableModel.setValueAt(loc.getCode_Postal(), ligne, 5);
+            tableModel.setValueAt(contLoc.getIdLocataire(), ligne, 0);
+            tableModel.setValueAt(contLoc.getDateDebutContrat(), ligne, 1);
+            tableModel.setValueAt(contLoc.getMontant(), ligne, 2);
+            tableModel.setValueAt(contLoc.getMontantLoyer(), ligne, 3);
+            tableModel.setValueAt(contLoc.getDateVersementLoyer(), ligne, 4);
+            tableModel.setValueAt(contLoc.getDateEntree(), ligne, 5);
+            tableModel.setValueAt(contLoc.getDateSortie(), ligne, 6);
+            tableModel.setValueAt(contLoc.getDepotDeGarantie(), ligne, 7);
+            tableModel.setValueAt(contLoc.getDateRevision(), ligne, 8);
+            tableModel.setValueAt(contLoc.getPeriodicitePaiement(), ligne, 9);
+            tableModel.setValueAt(contLoc.getDateFinContrat(), ligne, 10);
+            tableModel.setValueAt(contLoc.getChargesProvisionnelles(), ligne, 11);
+            tableModel.setValueAt(contLoc.getIdICC(), ligne, 12);
+            tableModel.setValueAt(contLoc.getValeurIcc(), ligne, 13);
+            tableModel.setValueAt(contLoc.getCaution(), ligne, 14);
+            tableModel.setValueAt(contLoc.getIdBienImm(), ligne, 15);
+    
 
             ligne++;
         }
